5d90d4bbff2a546bc332da5c6931cb3327eda55f
repl.it user <replituser@example.com>
2021-03-02 19:54:53 +0000

    Fixed the  formatting slightly

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ import requests
 import json
 from replit import db
 import discord
-
+from datetime import datetime
 
 
 class Weather:
@@ -26,35 +26,49 @@ class Weather:
           return "Error Code " + str(self.weather_info["cod"]) + ": " + self.weather_info["message"]
 
 
-  #CITY
-  #Description
-  #Temp:  / Feels_like:
-  #Temp_min:   / Temp_max:
-  #Humidity:   / Wind: m/second
+  #CITY, Description, Time
+  #Temp: 
+  #Feels_like:
+  #Wind: 
+  #Humidity:  
   def embed(self):
-
+    
+  ############  
+  #HELPERS
     kToC = lambda kelvin: round(kelvin - 273.15, 1)
     kToF = lambda kelvin: round(kToC(kelvin) * 9/5, 1) + 32
+    msTomph = lambda ms: round(ms * 2.237)
+    def compass(degree):
+      directions = [0, 45, 90, 135, 180, 225, 270, 315]
+      direction_dict = {0: "N°", 45: "NE°", 90: "E°", 135: "SE°", 180:"S°", 225:"SW°", 270:"W°", 315:"NW°"}
+      abs_dist = lambda list_degree: abs(degree - list_degree)
+      closest = min(directions, key = abs_dist)
+      return direction_dict[closest]
+  ############    
 
-    if(self.weather_info["cod"] == 200):
+    if(self.weather_info is not None and self.weather_info["cod"] == 200):
+      #SETTING VARIABLES
       fTemp = str(kToF(self.weather_info["main"]["temp"]))+ "°F"
       cTemp = str(kToC(self.weather_info["main"]["temp"]))+ "°C"
       fTempFeels = str(kToF(self.weather_info["main"]["feels_like"]))+ "°F"
       cTempFeels = str(kToC(self.weather_info["main"]["feels_like"]))+ "°C"
-      fTempMin = str(kToF(self.weather_info["main"]["temp_min"]))+ "°F"
-      cTempMin = str(kToC(self.weather_info["main"]["temp_min"]))+ "°C"
-      fTempMax = str(kToF(self.weather_info["main"]["temp_max"]))+ "°F"
-      cTempMax = str(kToC(self.weather_info["main"]["temp_max"]))+ "°C"
-      humidity = str(self.weather_info["main"]["humidity"])+ "%"
+      humidity = str(self.weather_info["main"]["humidity"])+ "% "+" "+" "+" "+" "
+      windSpeed = str(msTomph(self.weather_info["wind"]["speed"])) + " mph"
+      windDirection = compass(self.weather_info["wind"]["deg"])
+      time_cur =  self.weather_info["dt"] + self.weather_info["timezone"]
+      time_cur = datetime.utcfromtimestamp(time_cur).strftime('%m/%d %H:%M')
 
-      tit = self.weather_info["name"]+ ", " + self.weather_info["weather"][0]['description']
 
-      desc = "`Temp_cur: `" + fTemp + " / " + cTemp + "   " + "`Feels_like: `" + fTempFeels + " / " + cTempFeels +"\n`Temp_min: `" + fTempMin + " / " + cTempMin + "    " +"`Temp_Max: `" + fTempMax + " / " + cTempMax + "\n`Humidity: `" + humidity
+
+      #TITLE CREATION
+      tit = self.weather_info["name"]+ ", " + self.weather_info["weather"][0]['description'].title() + ", " + time_cur
+      #DESCRIPTION CREATION
+      desc = "`Temp current:` " + fTemp + " / " + cTemp + " \n" + "`Feels like:` " + fTempFeels + " / " + cTempFeels +"\n" + "`Wind Speed:` " + windSpeed + " " +  windDirection+ "\n" + "`Humidity:` " + humidity
+      
       
 
-
-      e = discord.Embed(title= tit, description= desc)
-      e.set_image(url="http://openweathermap.org/img/wn/"+self.weather_info['weather'][0]['icon'] +"@2x.png")
+      e = discord.Embed(title= tit, description= desc, colour = discord.Colour.dark_gold())
+      e.set_thumbnail(url="http://openweathermap.org/img/wn/"+self.weather_info['weather'][0]['icon'] +"@2x.png")
       return [True, e]
     else:
       return [False, None]
